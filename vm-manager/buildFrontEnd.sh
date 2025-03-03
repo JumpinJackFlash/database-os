3bdcf1b8103b6c910a604fcf46104cecec611b06
Steve Guilford <gilly@hunter.asteriondb.local>
2025-03-03 14:05:58 -0800

    Update...

--- vm-manager/buildFrontEnd.sh
+++ vm-manager/buildFrontEnd.sh
@@ -8,4 +8,4 @@ rm vm-manager.tar
 set -e
 podman save -o vm-manager.tar localhost/vm-manager:latest
 podman container prune --force
-podman create --name vm-manager-container -p 3000:3000 -e AUTH_TRUST_HOST='true' -e DB_TWIG_URL='http://'`cat /etc/hostname`':8080/dbTwig' vm-manager
+podman create --name vm-manager-container -p 3000:3000 -e DB_TWIG_URL='http://'`cat /etc/hostname`':8080/dbTwig' vm-manager
