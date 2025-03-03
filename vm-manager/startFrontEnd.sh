0aa9edd5ef784206ea5f85eeb7c6fd824ea15f8b
Steve Guilford <gilly@hunter.asteriondb.local>
2025-03-03 14:14:16 -0800

    Spelling...

--- vm-manager/startFrontEnd.sh
+++ vm-manager/startFrontEnd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 loginctl enable-linger
 podman container prune -y
-podman run --name vm-manager-container -p 3000:3000 -e DB_TIWG_URL='http://'`cat /etc/hostname`':8080/dbTwig' -d vm-manager
+podman run --name vm-manager-container -p 3000:3000 -e DB_TWIG_URL='http://'`cat /etc/hostname`':8080/dbTwig' -d vm-manager
 podman ps
