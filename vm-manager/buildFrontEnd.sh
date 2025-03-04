d9907afbdda83179d08045e014a9de1c717dca86
Steve Guilford <gilly@hunter.asteriondb.local>
2025-03-03 20:37:13 -0800

    .

--- vm-manager/buildFrontEnd.sh
+++ vm-manager/buildFrontEnd.sh
@@ -6,6 +6,6 @@ podman build . -f Dockerfile.multi -t vm-manager
 set +e
 rm vm-manager.tar
 set -e
-podman save -o vm-manager.tar localhost/vm-manager:latest
-podman container prune --force
-podman create --name vm-manager-container -p 3000:3000 -e DB_TWIG_URL='http://'`cat /etc/hostname`':8080/dbTwig' vm-manager
+# podman save -o vm-manager.tar localhost/vm-manager:latest
+# podman container prune --force
+# podman create --name vm-manager-container -p 3000:3000 -e DB_TWIG_URL='http://'`cat /etc/hostname`':8080/dbTwig' vm-manager
