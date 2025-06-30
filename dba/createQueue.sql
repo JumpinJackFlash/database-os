431bc837a6f1a190754cc5956c1a79eee3d7bfa1
Steve Guilford <gilly@hunter.asteriondb.local>
2025-06-29 17:35:50 -0700

    x

--- dba/createQueue.sql
+++ dba/createQueue.sql
@@ -5,7 +5,7 @@ begin
     queue_payload_type => '&1..dbos$message_t');
 
   dbms_aqadm.create_queue(
-    queue_name => '&1..dbos$vm_monitorr_q',
+    queue_name => '&1..dbos$vm_monitor_q',
     queue_table => '&1..dbos$vm_monitor_qt');
 
   dbms_aqadm.start_queue('&1..dbos$vm_monitor_q');
