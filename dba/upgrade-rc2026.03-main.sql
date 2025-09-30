e95b8686084fc91860def8c920eea7dcaec3d384
Steve Guilford <gilly@hunter.asteriondb.local>
2025-09-30 15:08:57 -0700

    vBump and system migrate.

---
+++ dba/upgrade-rc2026.03-main.sql
@@ -0,0 +1,46 @@
+/*
+
+
+
+*/
+
+define dba_user = '&1'
+define dba_password = '&2'
+define db_name = '&3'
+define dbos_user = '&4'
+define elog_user = '&5'
+define runtime_user = 'dbos_runtime'
+define runtime_password = '#SecurityBySimplicity2020#'
+
+spool $HOME/asterion/oracle/database-os/dba/upgrade-rc2026.03-main.log
+
+whenever sqlerror exit
+connect &dba_user/"&dba_password"@"&db_name"
+
+set echo on
+
+alter session set current_schema = &dbos_user;
+whenever sqlerror continue
+
+REM  Put stuff between here.....
+
+REM  ...and here
+
+@loadPackages
+
+set echo on
+
+delete  from middle_tier_map
+ where  object_group in ('dbos');
+
+commit;
+
+@$HOME/asterion/oracle/database-os/dba/dbTwigData
+
+commit;
+
+
+prompt 
+spool off;
+
+exit;
