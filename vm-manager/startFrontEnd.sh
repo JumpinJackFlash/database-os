7720cb56abf23f020a688dd7f3ad00a1b5052cbd
Steve Guilford <gilly@hunter.asteriondb.local>
2026-08-03 13:39:51 -0700

    Migrate then prune.

--- vm-manager/startFrontEnd.sh
+++ vm-manager/startFrontEnd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 loginctl enable-linger
-podman container prune -y
 podman system migrate
+podman container prune -f
 podman run --name vm-manager-container --replace -p 3000:3000 -e DB_TWIG_URL='http://'`cat /etc/hostname`':8080/dbTwig' -d vm-manager
 podman ps
